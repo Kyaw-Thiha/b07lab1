4e81aff9aa5dcf76567ac245721979a2a7d6bced
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-09-22 23:06:40 -0400

    Modified Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,36 +1,170 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Polynomial {
-  private double[] coefficients;
+  private List<Double> coefficients;
+  private List<Integer> exponents;
 
   public Polynomial() {
-    this.coefficients = new double[] { 0.0 };
+    this.coefficients = new ArrayList<>();
+    this.exponents = new ArrayList<>();
   }
 
-  public Polynomial(double[] coefficients) {
+  public Polynomial(List<Double> coefficients, List<Integer> exponents) {
     this.coefficients = coefficients;
+    this.exponents = exponents;
+  }
+
+  public Polynomial(File file) {
+    this();
+
+    try {
+      Scanner reader = new Scanner(file);
+      int i = 0;
+      boolean isCoefficient = true;
+      boolean isCoefficientPositive = true;
+      int floatingPointFactor = 0; // Should be 0 when int, and >=1 when in floating part
+
+      while (reader.hasNext()) {
+        String data = reader.next();
+        if (data == "-" || data == "+") {
+          // Handling the + and - signs
+          isCoefficient = true;
+          i++;
+          isCoefficientPositive = (data == "+");
+
+        } else if (data == "x") {
+          // Handling the x
+          isCoefficient = false;
+          floatingPointFactor = 0;
+
+        } else if (data == ".") {
+          // Handling the floating points in coefficients
+          floatingPointFactor = 1;
+
+        } else {
+          // Append the elements first if they do not exist
+          if (this.coefficients.toArray().length <= i) {
+            this.coefficients.add(0.0);
+            this.exponents.add(0);
+          }
+
+          if (isCoefficient) {
+            // Adding the coefficient
+            double coefficient = Double.parseDouble(data);
+
+            if (!isCoefficientPositive) {
+              coefficient = -coefficient;
+            }
+
+            if (floatingPointFactor == 0) {
+              this.coefficients.set(i, this.coefficients.get(i) * 10 + coefficient);
+            } else {
+              this.coefficients.set(i, this.coefficients.get(i) + (coefficient * Math.pow(0.1, floatingPointFactor)));
+            }
+            floatingPointFactor++;
+          } else {
+            // Adding the exponent
+            int exponent = Integer.parseInt(data);
+            this.exponents.set(i, this.exponents.get(i) * 10 + exponent);
+          }
+        }
+      }
+
+      reader.close();
+    } catch (FileNotFoundException e) {
+      // Hey, file is not found!!!
+    }
   }
 
   public Polynomial add(Polynomial poly) {
-    if (poly.coefficients.length > this.coefficients.length) {
-      for (int i = 0; i < this.coefficients.length; i++) {
-        poly.coefficients[i] = this.coefficients[i] + poly.coefficients[i];
+    for (int i = 0; i < poly.exponents.toArray().length; i++) {
+      int index = this.exponents.indexOf(poly.exponents.get(i));
+
+      if (index == -1) {
+        // If not found
+        this.exponents.add(poly.exponents.get(i));
+        this.coefficients.add(poly.coefficients.get(i));
+      } else {
+        // If found
+        this.coefficients.set(index, this.coefficients.get(index) + poly.coefficients.get(i));
       }
-      return poly;
-    } else {
-      for (int i = 0; i < poly.coefficients.length; i++) {
-        this.coefficients[i] = this.coefficients[i] + poly.coefficients[i];
-      }
-      return this;
     }
 
+    return this;
+  }
+
+  public Polynomial multiply(Polynomial poly) {
+    for (int i = 0; i < poly.exponents.toArray().length; i++) {
+      int exponent = poly.exponents.get(i);
+      double coefficient = poly.coefficients.get(i);
+
+      for (int j = 0; j < this.exponents.toArray().length; j++) {
+        // Add the exponents and multiply the coefficients
+        int newExponent = this.exponents.get(j) + exponent;
+
+        if (this.exponents.contains(newExponent)) {
+          int index = this.exponents.indexOf(newExponent);
+          this.coefficients.set(index, this.coefficients.get(index) * coefficient);
+        } else {
+          this.exponents.add(newExponent);
+          this.coefficients.add(coefficient);
+        }
+      }
+
+    }
+
+    return this;
   }
 
   public double evaluate(double x) {
     double result = 0;
-    for (int i = 0; i < this.coefficients.length; i++) {
-      result += this.coefficients[i] * Math.pow(x, i);
+    for (int i = 0; i < this.coefficients.toArray().length; i++) {
+      int exponent = this.exponents.get(i);
+      double coefficient = this.coefficients.get(i);
+
+      result += coefficient * Math.pow(x, exponent);
     }
+
     return result;
+  }
+
+  public boolean saveToFile(String fileName) {
+    // Empty out the file
+    try (FileWriter writer = new FileWriter(fileName, false)) {
+      System.out.println("File '" + fileName + "' has been emptied.");
+    } catch (IOException e) {
+      System.err.println("Error emptying file: " + e.getMessage());
+    }
+
+    try {
+      PrintWriter writer = new PrintWriter(fileName);
+
+      for (int i = 0; i < this.coefficients.toArray().length; i++) {
+        double coefficient = this.coefficients.get(i);
+        int exponent = this.exponents.get(i);
+
+        if (coefficient > 0 && i != 0) {
+          writer.append("+");
+          // Negative sign is handled automatically
+        }
+        writer.append(Double.toString(coefficient));
+        writer.append("x");
+        writer.append(Integer.toString(exponent));
+      }
+
+      writer.flush();
+      writer.close();
+    } catch (Exception e) {
+      return false;
+    }
+    return true;
   }
 
   public boolean hasRoot(double x) {
